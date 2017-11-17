eca6d6012473d6633a93286d1493cb9b578926d5
Bence Konecsni <konecsni_bence@hotmail.com>
2017-11-17 23:14:51 +0100

    added getter, setter for types

--- src/main/java/com/bkonecsni/logicgame/domain/types/Types.java
+++ src/main/java/com/bkonecsni/logicgame/domain/types/Types.java
@@ -5,4 +5,12 @@ import java.util.List;
 public class Types<T extends Type> {
 
     List<T> typeList;
+
+    public List<T> getTypeList() {
+        return typeList;
+    }
+
+    public void setTypeList(List<T> typeList) {
+        this.typeList = typeList;
+    }
 }
